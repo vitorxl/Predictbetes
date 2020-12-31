ef2cc29f708e1af3205c6361307a603e14d29084
vitorxl <vitorxlima@gmail.com>
2020-12-31 12:50:39 -0300

    descobrir o erro

--- APIdiabetes/main.py
+++ APIdiabetes/main.py
@@ -8,14 +8,14 @@ modelo  = pickle.load(open("../dt_reduzido.sav", "rb"))
 
 @app.route("/")
 def home():
+    #return "Minha HOME"
     return render_template("main.html")
 
-#@app.route("/predict", methods = ["POST"])
-#def preditor():
-#    dados = request.get_json()
-#    dados_input = [dados[col] for col in features]
-#    resultado = modelo.predict([dados_input])
-#    return jsonify(resultado = resultado[0])
+@app.route("/", methods = ["POST"])
+def preditor():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in features]
+    resultado = modelo.predict([dados_input])
+    return jsonify(resultado = resultado[0])
 
-if __name__ == '__main__':
-    app.run(debug = True)
+app.run(debug = True)
