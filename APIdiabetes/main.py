ca9ec327249cbc9dd7bf97a8265c719d024d6a19
vitorxl <vitorxlima@gmail.com>
2021-01-03 12:30:52 -0300

    pasta com teste

--- APIdiabetes/main.py
+++ APIdiabetes/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -11,11 +13,13 @@ def home():
     #return "Minha HOME"
     return render_template("main.html")
 
-@app.route("/", methods = ["POST"])
+@app.route("/preditor", methods = ["POST"])
 def preditor():
-    dados = request.get_json()
-    dados_input = [dados[col] for col in features]
-    resultado = modelo.predict([dados_input])
-    return jsonify(resultado = resultado[0])
+    #dados = request.get_json()
+    #dados_input = [dados[col] for col in request.form]
+    #resultado = modelo.predict([dados_input])
+    #return jsonify(resultado = resultado[0])
+    req = 10
+    print(req)
 
 app.run(debug = True)
