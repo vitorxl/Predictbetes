af8ca5498b739f20776ac0e786bf0f000a480688
vitorxl <vitorxlima@gmail.com>
2021-01-07 11:58:02 -0300

    ajustes nas features do modelo

--- APIdiabetes/main.py
+++ APIdiabetes/main.py
@@ -1,11 +1,9 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 import pickle
-import numpy as np
 
 app = Flask(__name__)
 
-features = ["Age", "Gender_Male", "Polyuria_Yes", "Polydipsia_Yes", "Alopecia_Yes"]
-modelo  = pickle.load(open("../dt_reduzido.sav", "rb"))
+modelo  = pickle.load(open("../dt_reduzido.pkl", "rb"))
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
@@ -14,12 +12,18 @@ def home():
 
     if request.method == "POST":
         age = int(request.form["age"])
-        gender_male = int(request.form["gender_male"])
         polyuria_yes = int(request.form["polyuria_yes"])
         polydipsia_yes = int(request.form["polydipsia_yes"])
+        weakness_yes = int(request.form["weakness_yes"])
+        visual_blurring_yes = int(request.form["visual_blurring_yes"])
+        delayed_healing_yes = int(request.form["delayed_healing_yes"])
         alopecia_yes = int(request.form["alopecia_yes"])
-        resultado = modelo.predict([[age, gender_male, polyuria_yes, polydipsia_yes, alopecia_yes]])
+        obesity_yes = int(request.form["obesity_yes"])
+
+        resultado = modelo.predict([[age, polyuria_yes, polydipsia_yes, weakness_yes, visual_blurring_yes,delayed_healing_yes, alopecia_yes, obesity_yes]])
         return str(resultado)
 
-
 app.run(debug = True)
+
+
+features = ['Age', 'Polyuria_Yes', 'Polydipsia_Yes', 'weakness_Yes', 'visual blurring_Yes', 'delayed healing_Yes', 'Alopecia_Yes', 'Obesity_Yes']
